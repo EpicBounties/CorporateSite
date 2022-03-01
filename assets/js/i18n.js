e52cd7f222f92b6118f3f4e4ee72b4987defc748
Juan Jose Florez <juanjofr2002@gmail.com>
2022-03-01 14:10:12 -0500

    'Try epic' and 'Blog' buttons are added, modal windows are created for each section. Change of the slogan.

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -10,61 +10,79 @@
  * One or more alternative language to be autodetected from browser locale (eg. 'es')
  * Routes have a international code preffix (eg. 'https://site.com/es/about.html')
  */
-
-let userLang
-
-function langDetect () {
-  // get lang from localStorage
-  userLang = localStorage.getItem('userLang')
-
-  // if lang is not set
-  if (!userLang) {
-    // get locale from browser
-    // TODO: refactor to work with multiple languages, currently it's hardcoded
-
-    let browserLocale = navigator.language || navigator.userLanguage
-    // derive lang from locale
-
-    if (browserLocale.match(/es/)) {
-      userLang = 'es'
-      window.location.replace(document.location.origin + '/' + userLang )
-    } else {
-      userLang = 'en'
-      window.location.replace(document.location.origin)
-    }
-  }
+if (window.location.href.includes("/es/")) {
+  localStorage.setItem('userLang', "es");
+} else if (window.location.href.includes("/en/")) {
+  localStorage.setItem('userLang', "en");
+}
+else if (window.location.href.includes("/")) {
+  localStorage.setItem('userLang', "en");
 }
 
-function langRedirect (redirectLang) {
-  // set lang on localStorage
+// let userLang
 
-  localStorage.setItem('userLang', redirectLang)
+// function langDetect() {
+//   // get lang from localStorage
+//   userLang = localStorage.getItem('userLang')
 
-  // compute path
-  // TODO: refactor to work with multiple languages, currently it's hardcoded
+//   // if lang is not set
+//   if (!userLang) {
+//     // get locale from browser
+//     // TODO: refactor to work with multiple languages, currently it's hardcoded
 
-  //split url
-  var split_url = document.location.href.split('/')
-  // get url length
-  if (split_url.length > 4) {
-    var pageURL = document.location.href
-    var langRE = "$1/" + redirectLang + "/$2"
-    var redirectURL = pageURL.replace(/(.*)\/[a-z]{2}\/(.*)/, langRE)
-    if (redirectURL == document.location.origin + '/en/' ) {
-      // go to root page if redirect to english page
-      redirectURL = document.location.origin
-    }
-  }else {
-    if (redirectLang == 'es') {
-      var pageURL = document.location.href
-      var redirectURL = document.location.origin + '/' + redirectLang
-    }
+//     let browserLocale = navigator.language || navigator.userLanguage
+//     // derive lang from locale
+
+//     if (browserLocale.match(/es/)) {
+//       userLang = 'es'
+//       window.location.replace(document.location.origin + '/' + userLang)
+//     } else {
+//       userLang = 'en'
+//       window.location.replace(document.location.origin)
+//     }
+//   }
+// }
+
+function langRedirect(redirectLang) {
+  if (redirectLang.includes("en")) {
+    window.location.href = `/`
+    console.log(redirectLang);
+  }else if (redirectLang.includes("es")){
+    window.location.href = `/${redirectLang}/`;
   }
-
-  if (pageURL != redirectURL) {
-    window.location.replace(redirectURL)
-  }
+  //window.location.href = `/${redirectLang}/`;
 }
 
-langDetect()
-langRedirect(userLang)
+// function langRedirect (redirectLang) {
+//   // set lang on localStorage
+
+//   localStorage.setItem('userLang', redirectLang)
+
+//   // compute path
+//   // TODO: refactor to work with multiple languages, currently it's hardcoded
+
+//   //split url
+//   var split_url = document.location.href.split('/')
+//   // get url length
+//   if (split_url.length > 4) {
+//     var pageURL = document.location.href
+//     var langRE = "$1/" + redirectLang + "/$2"
+//     var redirectURL = pageURL.replace(/(.*)\/[a-z]{2}\/(.*)/, langRE)
+//     if (redirectURL == document.location.origin + '/en/' ) {
+//       // go to root page if redirect to english page
+//       redirectURL = document.location.origin
+//     }
+//   }else {
+//     if (redirectLang == 'es') {
+//       var pageURL = document.location.href
+//       var redirectURL = document.location.origin + '/' + redirectLang
+//     }
+//   }
+
+//   if (pageURL != redirectURL) {
+//     window.location.replace(redirectURL)
+//   }
+// }
+
+//langDetect()
+//langRedirect(userLang)
